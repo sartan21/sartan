db75004b475e5ee8bab4c2f6c5b5ccba8d108acc
sartan21 <43236751+sartan21@users.noreply.github.com>
2018-09-17 13:00:57 +0200

    ppp
    ppp

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,6 +1,23 @@
 <html>
+<head>
+<script src="scripts/jquery-1.11.2.min.js"></script>
+<script src="scripts/cosasvarias.js"></script>
+</head>
 <body>
 <h2>Hello World with OpenShift! Prueba cambio222222!!!!</h2>
+
+<!-- <input id="val1" onkeyup="cargador()" />  -->
+
+<form>
+    Número 1: <input id="num1" type="number" />
+    Número 2: <input id="num2" type="number" />
+    <br /><br />
+    <input type="button" value="SUMAR" onclick="operaciones('sumar'); return false;" /><br />
+    <input type="button" value="RESTAR" onclick="operaciones('restar'); return false;" /><br />
+    <input type="button" value="MULTIPLICAR" onclick="operaciones('multiplicar'); return false;" /><br />
+    <input type="button" value="DIVIDIR" onclick="operaciones('dividir'); return false;" />
+</form>
+<!--
 <img src="p1.jpg" title="Acceso a trámites" style="position: absolute; top: 20%; left: 85%;" width="80" height="110" align="right">
 <br>
 <img src="p2.jpg" title="Acceso a trámites" style="position: absolute; top: 20%; left: 85%;" width="80" height="110" align="right">
@@ -10,5 +27,6 @@
 <img src="p4.jpg" title="Acceso a trámites" style="position: absolute; top: 20%; left: 85%;" width="80" height="110" align="right">
 <br>
 <img src="p5.jpg" title="Acceso a trámites" style="position: absolute; top: 20%; left: 85%;" width="80" height="110" align="right">
+-->
 </body>
 </html>
---
+++ src/main/webapp/scripts/cosasvarias.js
@@ -0,0 +1,63 @@
+function operaciones(op)
+{
+
+    var ops = {
+        sumar: function sumarNumeros(n1, n2) {
+            return (parseInt(n1) + parseInt(n2));
+        },
+
+        restar: function restarNumeros(n1, n2) {
+            return (parseInt(n1) - parseInt(n2));
+        },
+        
+        multiplicar: function multiplicarNumeros(n1, n2) {
+            return (parseInt(n1) * parseInt(n2));
+        },
+
+        dividir: function dividirNumeros(n1, n2) {
+            return (parseInt(n1) / parseInt(n2));
+        }
+
+
+    };
+
+
+
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+
+    
+    //Comprobamos si se ha introducido números en las cajas
+    if (isNaN(parseFloat(document.getElementById('num1').value))) {
+        alert("Indique un número en 'numero1'");
+        document.getElementById("num1").innerText = "0";
+        document.getElementById("num1").focus();
+        } else if (isNaN(parseFloat(document.getElementById('num2').value))) {
+        alert("Indique un número en 'numero2'");
+        document.getElementById("num2").innerText = "0";
+        document.getElementById("num2").focus();
+    }
+    else {
+    //Si se han introducido los números en ámbas cajas, operamos:
+        switch(op) {
+            case 'sumar':
+                var resultado = ops.sumar(num1, num2);
+                alert (resultado);
+                break;
+            case 'restar':
+                var resultado = ops.restar(num1, num2);
+                alert (resultado);
+                break;
+            case 'multiplicar':
+                var resultado = ops.multiplicar(num1, num2);
+                alert(resultado);
+                break;
+            case 'dividir':
+                var resultado = ops.dividir(num1, num2);
+                alert(resultado);
+                break;
+
+        }
+    }
+
+}
